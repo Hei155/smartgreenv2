926068bd29879653abb212e9bde3411a0ab341c1
Igor Romanov <Hei155@yandex.ru>
2022-10-25 21:35:41 +0300

    Link

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -13,14 +13,15 @@ export const Main = (props) => {
                 </div>
                 <div className="processed processed_hidden" ref={props.processedRef}>
                     <form className="processed__form" onSubmit={props.handleProcessedButton}>
-                        <label className="processed__label" htmlFor="initial">Введите строку, с которой будет вестись расчёт</label>
+                        <label className="processed__label" htmlFor="initial">Введите строку, с которой будет вестись расчёт (начиная со строки 8)</label>
                         <input className="processed__input" name="initial" type="number" onChange={props.handleChangeFirst}></input>
                         <label className="processed__label" htmlFor="final">Введите строку, по которую будет вестись расчёт</label>
                         <input className="processed__input" name="final" type="number" onChange={props.handleChangeSecond}></input>
                         <button className="processed__buton">Получить данные</button>
-                        <span className="processed__data">Сумма C02: {props.us}</span>
-                        <span className="processed__data">Сумма C02 на пассажира: {props.usPas}</span>
-                        <span className="processed__data">Углеродный след: {props.gasFootprint}</span>
+                        <h3>Расчёт для выбранного участка дороги:</h3>
+                        <span className="processed__data">Углеродный след автобуса: {props.us}</span>
+                        <span className="processed__data">Углеродный след на одного пассажира: {props.usPas}</span>
+                        <span className="processed__data">Углеродный след  автомобиля: {props.gasFootprint}</span>
                     </form>
                 </div>
             </section>
